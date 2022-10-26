39c64a9f571aeaa667a53dc24614e1f3cadce83e
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-10-26 16:25:46 +0700

    add method binary search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -38,6 +38,37 @@ namespace SearchAlgorithm
                 arr[i] = Int32.Parse(s1);
             }
         }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //accept thenumber to be search
+                Console.Write
+                    ("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                // loop to search for the elements in the array
+                while ((item !=arr[mid]) && (lowerbound<= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+            }
+        }
         static void Main(string[] args)
         {
         }
